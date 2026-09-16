b6b2323f4c0331203f93c98ec54b3dfbfd786a4e
Ayush Pathak <ayushpathak00851@gmail.com>
2026-09-16 17:50:29 +0530

    Add new origin to CORS configuration

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://honeyassist.onrender.com" ,
+        "https://honey-assist-dlxc.vercel.app/" ,
     ],
     allow_credentials=True,
     allow_methods=["*"],
