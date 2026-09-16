edf96648383dfa11381963ab3f93e02a9c0cdf86
Ayush Pathak <ayushpathak00851@gmail.com>
2026-09-16 18:17:26 +0530

    Update allowed origins for CORS middleware

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://honey-assist-dlxc.vercel.app" ,
+        "https://honey-assist-hij4.vercel.app" ,
     ],
     allow_credentials=True,
     allow_methods=["*"],
